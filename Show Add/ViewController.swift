85c872430e519a462d565cf40962bc154c64ee9e
MasterUNG <phrombutr@gmail.com>
2017-09-09 14:17:03 +0700

    OK

--- Show Add/ViewController.swift
+++ Show Add/ViewController.swift
@@ -9,6 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Implicit
+    var xString = ""
+    var yString = ""
+    
+    
+    
+    
+    @IBOutlet weak var xOutlet: UITextField!
+    
+    
+    @IBOutlet weak var yOutlet: UITextField!
+    
+    
+    @IBOutlet weak var answerOutlet: UILabel!
+    
+    
+    
+    @IBAction func calButton(_ sender: Any) {
+        
+        //Get Value From TextField
+        xString = xOutlet.text!
+        yString = yOutlet.text!
+        
+        //Show Log
+        print("x ==> \(xString)")
+        print("y ==> \(yString)")
+        
+        //Change Data Type
+        let xAInt = Int(xString)
+        let yAInt = Int(yString)
+        
+        let answerAInt = xAInt! + yAInt!
+        let answerString = String(answerAInt)
+        answerOutlet.text = answerString
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
